54bb16121c9a470838ce5794c18341982eef1309
ArcadeFortune <lamaa@bzz.ch>
2023-07-05 10:34:10 +0200

    feat: added /tasks endpoint

--- Backend/accessDB.js
+++ Backend/accessDB.js
@@ -1,16 +1,14 @@
 const client = require("./connection.js");
+
 async function withDatabase(callback) {
-  console.log('in accessdb')
   try {
-    console.log('in try')
     await client.connect();
     const db = await client.db("todolist");
     const coll = db.collection("tasks");
-    // const myDB = client.db("myDB");
     
     await callback(coll)
+
   } finally {
-    console.log('in finally')
     await client.close();
   }
 }
--- Backend/server.js
+++ Backend/server.js
@@ -15,19 +15,33 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
   console.log('in endpoint')
-  withDatabase(async (coll) => {
-    console.log('in function')
-    const res = await coll.find(); // no query
-    for await (const doc of res) {
-      console.log(doc);
-    }
-    const customIdDocument = { _id: "3", key: "cleanign" };
-    await coll.insertOne(customIdDocument);
-    console.log(coll, 'htset')
-  })
+  // await withDatabase(async (coll) => {
+  //   console.log('in function')
+  //   const res = await coll.find();
+  //   for await (const doc of res) {
+  //     console.log(doc);
+  //   }
+  //   const customIdDocument = { _id: "3", key: "cleanign" };
+  //   await coll.insertOne(customIdDocument);
+  //   console.log(coll, 'htset')
+  // })
   
   res.json({ message: "Welcome to ArcadeFortune application!!!!\nWatch Date A Live"});
 });
+
+app.get('/tasks', async (req, res) => {
+  let result = []
+  await withDatabase(async (coll) => {
+    const find = await coll.find().sort({ _id: 1 });
+    for await (const doc of find) {
+      console.log(doc)
+      result.push(doc)
+    }
+  })
+  res.json(result)
+})
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
