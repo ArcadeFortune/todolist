c6a8bfa0072522273dc248211c80faf78bae71ce
ArcadeFortune <alessionights@gmail.com>
2023-07-05 21:52:05 +0200

    dev: no more rebuilding image to update changes

---
+++ frontend/docker-entrypoint.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+#!/bin/sh
+# vim:sw=4:ts=4:et
+
+set -e
+
+if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
+    exec 3>&1
+else
+    exec 3>/dev/null
+fi
+
+if [ "$1" = "nginx" -o "$1" = "nginx-debug" ]; then
+    if /usr/bin/find "/docker-entrypoint.d/" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read v; then
+        echo >&3 "$0: /docker-entrypoint.d/ is not empty, will attempt to perform configuration"
+
+        echo >&3 "$0: Looking for shell scripts in /docker-entrypoint.d/"
+        find "/docker-entrypoint.d/" -follow -type f -print | sort -n | while read -r f; do
+            case "$f" in
+                *.sh)
+                    if [ -x "$f" ]; then
+                        echo >&3 "$0: Launching $f";
+                        "$f"
+                    else
+                        # warn on shell scripts without exec bit
+                        echo >&3 "$0: Ignoring $f, not executable";
+                    fi
+                    ;;
+                *) echo >&3 "$0: Ignoring $f";;
+            esac
+        done
+
+        echo >&3 "$0: Configuration complete; ready for start up"
+    else
+        echo >&3 "$0: No files found in /docker-entrypoint.d/, skipping configuration"
+    fi
+fi
+
+# Set up endpoint for env retrieval
+echo "window._env_ = {" > /usr/share/nginx/html/env_config.js
+
+# Collect enviroment variables for react
+eval enviroment_variables="$(env | grep REACT_APP.*=)"
+
+# Loop over variables
+env | grep REACT_APP.*= | while read -r line; 
+do
+    printf "%s',\n" $line | sed "s/=/:'/" >> /usr/share/nginx/html/env_config.js
+    
+    # Notify the user
+    printf "Env variable %s' was injected into React App. \n" $line | sed "0,/=/{s//:'/}"
+
+done
+
+# End the object creation
+echo "}" >> /usr/share/nginx/html/env_config.js
+
+echo "Enviroment Variable Injection Complete."
+
+
+exec "$@"
